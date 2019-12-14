cb6e9d32f7c06724f60c7ca7957e0b86038fbff8
Andrei Vajna II <AndreiVajnaII@gmail.com>
2019-12-14 13:07:49 +0200

    Day 12 complete

--- 12.ts
+++ 12.ts
@@ -10,14 +10,13 @@ class Vector {
     get energy() {
         return axes.map(axis => Math.abs(this[axis])).reduce((r, x) => r + x);
     }
+
 }
 
 class Moon {
-    public velocity = new Vector(0, 0, 0);
-
     constructor(
         public position: Vector,
-    ) { }
+        public velocity = new Vector(0, 0, 0)) { }
 
     get potentialEnergy() {
         return this.position.energy;
@@ -45,17 +44,62 @@ function applyVelocity(m: Moon) {
     });
 }
 
+class Pattern {
+    public found = false;
+    private history: number[] = [];
+
+    constructor(private getValue: (moons: Moon[]) => number) { }
+
+    get length() {
+        return this.history.length / 2;
+    }
+
+    public check(moons: Moon[]) {
+        this.history.push(this.getValue(moons));
+        return this.checkValues();
+    }
+
+    private checkValues() {
+        if (this.history.length % 2 === 0) {
+            for (let i = 0; i < this.history.length / 2; i++) {
+                if (this.history[i] !== this.history[this.history.length / 2 + i]) {
+                    return;
+                }
+            }
+            this.found = true;
+        }
+    }
+
+}
+
 export function solve(lines: string[]) {
     const moons = lines.map(parseVector).map(v => new Moon(v));
-    for (let step = 0; step < 1000; step++) {
+    const patterns: Pattern[] = [];
+    for (let i = 0; i < moons.length; i++) {
+        patterns.push(...axes.map(axis => new Pattern(m => m[i].position[axis])));
+        patterns.push(...axes.map(axis => new Pattern(m => m[i].velocity[axis])));
+    }
+    patterns.forEach(p => {
+        p.check(moons);
+    });
+    let step = 0;
+    do {
+        step++;
         for (let i = 0; i < moons.length - 1; i++) {
             for (let j = i + 1; j < moons.length; j++) {
                 applyGravity(moons[i], moons[j]);
             }
         }
         moons.forEach(applyVelocity);
-    }
-    return moons.map(m => m.totalEnergy).reduce((r, x) => r + x);
+        patterns.filter(p => !p.found).forEach(p => {
+            p.check(moons);
+        });
+        if (step % 100 === 0) {
+            console.log(step, `Found  ${patterns.filter(p => p.found).length}/${patterns.length} cycles`);
+        }
+    } while (!patterns.every(p => p.found));
+    console.log(step);
+    return mlcm(patterns.map(p => p.length));
 }
 
 function parseVector(line: string) {
@@ -64,4 +108,19 @@ function parseVector(line: string) {
         throw new Error("error parsing input");
     }
     return new Vector(+m[1], +m[2], + m[3]);
+}
+
+function mlcm(values: number[]) {
+    return values.reduce((r, x) => lcm(r, x));
+}
+
+function lcm(a: number, b: number) {
+    return a * b / gcd(a, b);
+}
+
+function gcd(a: number, b: number): number {
+    while (b !== 0) {
+        [a, b] = [b, a % b];
+    }
+    return a;
 }
