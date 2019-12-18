fffa11a22ef5bff96f393a2e60d5d5483229d3f8
Andrei Vajna II <AndreiVajnaII@gmail.com>
2019-12-18 17:30:08 +0200

    Day 16 - trying something for part 2

--- 16.ts
+++ 16.ts
@@ -1,23 +1,46 @@
 export function solve(lines: string[]) {
-    let input = lines[0].split("").map(s => +s);
-    for (let i = 0; i < 100; i++) {
-        input = phase(input);
-    }
-    return input.slice(0, 8).join("");
+    // lines = ["12345678"];
+    const input = lines[0].split("").map(s => +s);
+    return [0, 1, 2, 3, 4, 5, 6, 7].map(i => calc(i, 100, input)).join("");
 }
 
-function phase(input: number[]) {
-    return input.map(calcOutput);
+const cache: number[][] = [];
+
+function calc(index: number, phase: number, input: number[]): number {
+    if (cache[phase] && cache[phase][index] !== undefined) {
+        return cache[phase][index];
+    }
+    let v = 0;
+    for (let i = index; i < input.length; i += index + 1) {
+        for (let j = 0; j < index + 1 && i < input.length; j++ , i++) {
+            v += phase > 1 ? calc(i, phase - 1, input) : input[i];
+        }
+        i += index + 1;
+        for (let j = 0; j < index + 1 && i < input.length; j++ , i++) {
+            v -= phase > 1 ? calc(i, phase - 1, input) : input[i];
+        }
+    }
+    v = Math.abs(v) % 10;
+    addToCache(phase, index, v);
+    return v;
 }
+
+function addToCache(phase: number, index: number, v: number) {
+    if (!cache[phase]) {
+        cache[phase] = [];
+    }
+    cache[phase][index] = v;
+}
+
 
 function calcOutput(x: number, index: number, input: number[]) {
     let v = 0;
     for (let i = index; i < input.length;) {
-        for (let j = 0; j < index + 1 && i < input.length; j++, i++) {
+        for (let j = 0; j < index + 1 && i < input.length; j++ , i++) {
             v += input[i];
         }
         i += index + 1;
-        for (let j = 0; j < index + 1 && i < input.length; j++, i++) {
+        for (let j = 0; j < index + 1 && i < input.length; j++ , i++) {
             v -= input[i];
         }
         i += index + 1;
