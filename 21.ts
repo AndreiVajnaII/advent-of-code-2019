bb561aeb83f54f3eb83900d71a5d2c7b2a7432b1
Andrei Vajna II <andreivajnaii@gmail.com>
2019-12-23 21:20:54 +0200

    Day 21 - complete

--- 21.ts
+++ 21.ts
@@ -1,53 +1,44 @@
 import { AsciiIO, IntcodeProcessor } from "./intcode";
 
-const registers = "ABCD".split("");
-
 export function solve(lines: string[]) {
     const program = lines[0].split(",").map(s => +s);
     const droid = new SpringDroid(program);
-    droid.run();
-    const test = generateTest([true, true, true, true]);
-    return droid.runSpring([
-        ...test,
-        "WALK",
-    ]);
+    const spring1 = [
+        "NOT A J",
+        "NOT B T",
+        "OR T J",
+        "NOT C T",
+        "OR T J",
+        "AND D J",
+        "WALK"];
+    const spring2 = [
+        "NOT A J",
+        "NOT B T",
+        "OR T J",
+        "NOT C T",
+        "OR T J",
+        "AND D J",
+        "NOT E T",
+        "NOT T T",
+        "OR H T",
+        "AND T J",
+        "RUN"];
+    return [droid.run(spring1), droid.run(spring2)];
 }
 
 class SpringDroid {
-    private io: AsciiIO;
-    private droid: IntcodeProcessor;
+    constructor(private program: number[]) { }
 
-    constructor(program: number[]) {
-        this.io = new AsciiIO();
-        this.droid = new IntcodeProcessor(program, this.io);
-    }
-
-    public run() {
-        this.droid.run();
-        console.log(this.io.readString());
-    }
-
-    public runSpring(instructions: string[]) {
-        this.io.writeInstruction(instructions.join("\n"));
-        this.run();
-        return this.io.shift();
-    }
-}
-
-function generateTest(config: boolean[]) {
-    const instructions: string[] = [];
-    if (config[0]) {
-        instructions.push(`OR A J`);
-    } else {
-        instructions.push(`NOT A J`);
-    }
-    for (let i = 1; i < config.length; i++) {
-        if (config[i]) {
-            instructions.push(`OR ${registers[i]} J`);
-        } else {
-            instructions.push(`NOT ${registers[i]} T`);
-            instructions.push("AND T J");
+    public run(instructions: string[]) {
+        const io = new AsciiIO();
+        const droid = new IntcodeProcessor([...this.program], io);
+        io.writeInstruction(instructions.join("\n"));
+        droid.run();
+        console.log(io.readString());
+        try {
+            return io.shift();
+        } catch (e) {
+            return;
         }
     }
-    return instructions;
 }
