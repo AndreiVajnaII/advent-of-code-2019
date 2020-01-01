03b169a07afe8f0adf6efc14d1b42064de4b090d
Andrei Vajna II <andreivajnaii@gmail.com>
2020-01-01 15:00:58 +0200

    Day 25 - part 1

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-import { solve } from "./18";
+import { solve } from "./25";
 import { readInput } from "./helpers";
 
-const inputUrl = "https://adventofcode.com/2019/day/18/input";
+const inputUrl = "https://adventofcode.com/2019/day/25/input";
 
 readInput(inputUrl, process.argv[2], solve);
