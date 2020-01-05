9fd2d9c51113334e6b1a14df882e945ce6fbc9cf
Andrei Vajna II <AndreiVajnaII@gmail.com>
2020-01-05 23:54:31 +0200

    Day 16 - complete

--- 16.ts
+++ 16.ts
@@ -1,39 +1,39 @@
 export function solve(lines: string[]) {
-    // lines = ["12345678"];
     const input = lines[0].split("").map(s => +s);
-    return [0, 1, 2, 3, 4, 5, 6, 7].map(i => calc(i, 100, input)).join("");
+    const result1 = fft(input, 100).slice(0, 8).join("");
+    const result2 = part2(input, 100).join("");
+    return [result1, result2];
 }
 
-const cache: number[][] = [];
-
-function calc(index: number, phase: number, input: number[]): number {
-    if (cache[phase] && cache[phase][index] !== undefined) {
-        return cache[phase][index];
+function part2(input: number[], phases: number) {
+    /*  The offset is close enough to the end that it can be calculated just by adding up
+        all values of the phase below, starting with the offset up to the end. */
+    const offset = +input.slice(0, 7).join("");
+    const count = 10000 * input.length - offset;
+    const values: number[][] = new Array(count);
+    for (let i = 0; i < count; i++) {
+        values[i] = [input[input.length - i % input.length - 1]];
     }
-    let v = 0;
-    for (let i = index; i < input.length; i += index + 1) {
-        for (let j = 0; j < index + 1 && i < input.length; j++ , i++) {
-            v += phase > 1 ? calc(i, phase - 1, input) : input[i];
+    for (let p = 1; p <= phases; p++) {
+        values[0][p] = values[0][0];
+    }
+    for (let i = 1; i < count; i++) {
+        for (let p = 1; p <= phases; p++) {
+            values[i][p] = (values[i][p - 1] + values[i - 1][p]) % 10;
         }
-        i += index + 1;
-        for (let j = 0; j < index + 1 && i < input.length; j++ , i++) {
-            v -= phase > 1 ? calc(i, phase - 1, input) : input[i];
-        }
     }
-    v = Math.abs(v) % 10;
-    addToCache(phase, index, v);
-    return v;
+    return values.map(arr => arr[100]).slice(count - 8).reverse();
 }
 
-function addToCache(phase: number, index: number, v: number) {
-    if (!cache[phase]) {
-        cache[phase] = [];
+function fft(input: number[], phases: number) {
+    while (phases > 0) {
+        phases--;
+        input = input.map(calcOutput);
     }
-    cache[phase][index] = v;
+    return input;
 }
 
-
-function calcOutput(x: number, index: number, input: number[]) {
+function calcOutput(_: number, index: number, input: number[]) {
     let v = 0;
     for (let i = index; i < input.length;) {
         for (let j = 0; j < index + 1 && i < input.length; j++ , i++) {
