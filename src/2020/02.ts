a4b37e0bd988d4a6d30616e20e1c713b8c73de86
Andrei Vajna II <AndreiVajnaII@gmail.com>
2020-12-02 09:34:50 +0200

    2020 - day 2

---
+++ src/2020/02.ts
@@ -0,0 +1,26 @@
+export function solve(lines: string[]) {
+    return [
+        lines.filter(isValid1).length,
+        lines.filter(isValid2).length,
+    ];
+}
+
+function isValid1(line: string) {
+    const [min, max, letter, password] = parseLine(line);
+    const count = password.split("").filter(c => c === letter).length;
+    return min <= count && count <= max;
+}
+
+function isValid2(line: string) {
+    const [first, second, letter, password] = parseLine(line);
+    const isFirst = password.charAt(first - 1) === letter;
+    const isSecond = password.charAt(second - 1) === letter;
+    return (isFirst && !isSecond) || (!isFirst && isSecond);
+}
+
+function parseLine(line: string) {
+    const [policy, password] = line.split(": ");
+    const [range, letter] = policy.split(" ");
+    const [first, second] = range.split("-").map(s => +s);
+    return [first, second, letter, password] as const;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { solve } from "./2020/01";
+import { solve } from "./2020/02";
 import { readInput } from "./helpers";
 
-const inputUrl = "https://adventofcode.com/2020/day/1/input";
+const inputUrl = "https://adventofcode.com/2020/day/2/input";
 
 readInput(inputUrl, process.argv[2], solve);
