f32f70bac1f6631abefaa0d86e59e01288b4ba97
Andrei Vajna II <AndreiVajnaII@gmail.com>
2020-12-03 09:41:40 +0200

    2020 - Day 3

---
+++ src/2020/03.ts
@@ -0,0 +1,15 @@
+export function solve(map: string[]) {
+    return [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]]
+        .map(([slopeY, slopeX]) => countTrees(map, slopeY, slopeX))
+        .reduce((r, x) => r * x);
+}
+
+function countTrees(map: string[], slopeY: number, slopeX: number) {
+    let count = 0;
+    for (let y = 0, x = 0; y < map.length; y += slopeY, x = (x + slopeX) % map[0].length) {
+        if (map[y][x] === "#") {
+            count++;
+        }
+    }
+    return count;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { solve } from "./2020/02";
+import { solve } from "./2020/03";
 import { readInput } from "./helpers";
 
-const inputUrl = "https://adventofcode.com/2020/day/2/input";
+const inputUrl = "https://adventofcode.com/2020/day/3/input";
 
 readInput(inputUrl, process.argv[2], solve);
