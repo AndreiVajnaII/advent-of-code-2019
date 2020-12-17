d6d8c322e230cb64cff11a0698bfad2f10d7b0e8
Andrei Vajna II <AndreiVajnaII@gmail.com>
2020-12-17 15:49:14 +0200

    2020 - Day 17 - part 1

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { solve } from "./2020/16";
+import { solve } from "./2020/17";
 import { readInput } from "./helpers";
 
-const inputUrl = "https://adventofcode.com/2020/day/16/input";
+const inputUrl = "https://adventofcode.com/2020/day/17/input";
 
 readInput(inputUrl, process.argv[2], solve);
